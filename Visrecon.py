a61d8bed8e66b25d72f2ede385c61cd49d4daff2
SvenDijkstra <54924612+SvenDijkstra@users.noreply.github.com>
2023-04-07 17:18:25 +0200

    Update Visrecon.py

--- Visrecon.py
+++ Visrecon.py
@@ -9,8 +9,28 @@ from PIL import Image
 import numpy as np
 import cv2
 
+# Define global variables
+drawing = False
+last_point = None
+canvas = None
+
+def handle_mouse(event, x, y, flags, param):
+    global drawing, last_point, canvas
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        last_point = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        last_point = None
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            if last_point is not None:
+                cv2.line(canvas, last_point, (x, y), 255, 3)
+            last_point = (x, y)
+            
 # Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 # Preprocess the data
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
@@ -20,38 +40,55 @@ x_train /= 255
 x_test /= 255
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
+
+# # Define the model
+# model = Sequential()
+# model.add(Dense(64, activation='relu', input_shape=(784,)))
+# model.add(Dense(10, activation='softmax'))
+
+# Create a strong model with 4 layers and 128 neurons
 # Define the model
 model = Sequential()
-model.add(Dense(64, activation='relu', input_shape=(784,)))
+model.add(Dense(128, activation='relu', input_shape=(784,)))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(10, activation='softmax'))
+
 
 # Compile the model
 sgd = SGD(learning_rate=0.1)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
+
+# Ask user for number of epochs
+print("Please enter the number of epochs to train the model (reasonable range: 10-30): ")
+num_epochs = int(input())
+
 # Train the model
 model.fit(x_train, y_train,
           batch_size=128,
-          epochs=20,
+          epochs=num_epochs, # use user input here
           validation_data=(x_test, y_test))
 
-# Ask user to draw 3 letters and convert them into MNIST-like format
+# Ask user to draw 3 digits and convert them into MNIST-like format
 user_input = []
 for i in range(3):
     # Display instructions to user
-    print("Draw letter #" + str(i+1) + ": ")
+    print("Draw digit #" + str(i+1) + ": ")
     print("(Press 's' to save and 'q' to quit)")
-
+    
     # Create a black canvas
     canvas = np.zeros((200,200), dtype=np.uint8)
-
+    
     # Allow user to draw on canvas
     drawing = False
     last_point = None
     cv2.namedWindow('Draw')
+    cv2.setMouseCallback('Draw', handle_mouse, canvas)  # register handle_mouse as callback function
     cv2.createTrackbar('X', 'Draw', 0, 200, lambda x: x)
     cv2.createTrackbar('Y', 'Draw', 0, 200, lambda x: x)
+
     while True:
         cv2.imshow('Draw', canvas)
         k = cv2.waitKey(1) & 0xFF
@@ -66,26 +103,8 @@ for i in range(3):
             if len(user_input) > 0:
                 user_input.pop()
             break
-
-
-        # # Get the current position of the mouse
-        # current_point = (cv2.getTrackbarPos('X','Draw'), cv2.getTrackbarPos('Y','Draw'))
-
-        # # Start drawing when the left mouse button is pressed
-        # if k == ord('a'):
-            # drawing = True
-            # last_point = current_point
-
-        # # Stop drawing when the left mouse button is released
-        # elif k == ord('d'):
-            # drawing = False
-
-        # # Draw a line if the left mouse button is pressed
-        # if drawing:
-            # if last_point is not None:
-                # cv2.line(canvas, last_point, current_point, 255, 3)
-            # last_point = current_point
-
+        
+        
     # Preprocess the user input and add it to the list
     img = Image.fromarray(canvas)
     img = img = img.resize((28,28), resample=Image.LANCZOS) # use Image.LANCZOS for resampling
@@ -95,32 +114,19 @@ for i in range(3):
     img /= 255
     user_input.append(img)
 
-
-def handle_mouse(event, x, y, flags, param, canvas):
-    global drawing, last_point
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        last_point = (x, y)
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        last_point = None
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing:
-            if last_point is not None:
-                cv2.line(canvas, last_point, (x, y), 255, 3)
-            last_point = (x, y)
-
+            
 # Convert the user input into a numpy array
 user_input = np.array(user_input)
 
 # Predict the user input using the trained model
 predictions = model.predict(user_input)
 
-# Convert predictions to letters
-letters = [chr(np.argmax(prediction) + 97) for prediction in predictions]
+# Convert predictions to digits
+digits = [str(np.argmax(prediction)) for prediction in predictions]
+
 
 # Display the predictions
-print("The model predicts that you drew the letters: " + " ".join(letters))
+print("The model predicts that you drew the digits: " + " ".join(digits))
 
 # Display the user input
 fig, ax = plt.subplots(1, 3, figsize=(5, 5))
