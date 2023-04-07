c855b7e8470fc175466640fb95e126dea52dec48
SvenDijkstra <54924612+SvenDijkstra@users.noreply.github.com>
2023-04-07 17:13:25 +0200

    Update Visrecon.py

--- Visrecon.py
+++ Visrecon.py
@@ -11,7 +11,6 @@ import cv2
 
 # Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 # Preprocess the data
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
@@ -21,7 +20,6 @@ x_train /= 255
 x_test /= 255
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
-
 # Define the model
 model = Sequential()
 model.add(Dense(64, activation='relu', input_shape=(784,)))
@@ -32,7 +30,6 @@ sgd = SGD(learning_rate=0.1)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
-
 # Train the model
 model.fit(x_train, y_train,
           batch_size=128,
@@ -45,10 +42,10 @@ for i in range(3):
     # Display instructions to user
     print("Draw letter #" + str(i+1) + ": ")
     print("(Press 's' to save and 'q' to quit)")
-    
+
     # Create a black canvas
     canvas = np.zeros((200,200), dtype=np.uint8)
-    
+
     # Allow user to draw on canvas
     drawing = False
     last_point = None
@@ -69,8 +66,8 @@ for i in range(3):
             if len(user_input) > 0:
                 user_input.pop()
             break
- 
-    
+
+
         # # Get the current position of the mouse
         # current_point = (cv2.getTrackbarPos('X','Draw'), cv2.getTrackbarPos('Y','Draw'))
 
@@ -88,7 +85,7 @@ for i in range(3):
             # if last_point is not None:
                 # cv2.line(canvas, last_point, current_point, 255, 3)
             # last_point = current_point
-            
+
     # Preprocess the user input and add it to the list
     img = Image.fromarray(canvas)
     img = img = img.resize((28,28), resample=Image.LANCZOS) # use Image.LANCZOS for resampling
@@ -97,8 +94,8 @@ for i in range(3):
     img = img.astype('float32')
     img /= 255
     user_input.append(img)
-    
-   
+
+
 def handle_mouse(event, x, y, flags, param, canvas):
     global drawing, last_point
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -112,7 +109,7 @@ def handle_mouse(event, x, y, flags, param, canvas):
             if last_point is not None:
                 cv2.line(canvas, last_point, (x, y), 255, 3)
             last_point = (x, y)
-            
+
 # Convert the user input into a numpy array
 user_input = np.array(user_input)
 
@@ -131,5 +128,3 @@ for i in range(3):
     ax[i].imshow(np.reshape(user_input[i], (28, 28)), cmap=plt.get_cmap('gray'))
     ax[i].set_title("Letter #" + str(i+1))
 plt.show()
-
-
